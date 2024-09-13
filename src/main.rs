c714a2d9d71dfe3dc501b181197da568a2ef23f6
Doran Penner <doranaviv@gmail.com>
2024-09-13 19:22:43 +0200

    Cleanly handle missing arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::env;
-use std::fs;
+use std::{ env, fs, process };
 
 /* Notes
  * I'm doing the arg parsing differently than the guide,
@@ -12,20 +11,23 @@ struct Config {
 }
 
 impl Config {
-    fn new(mut args: env::Args) -> Self {
+    fn build(mut args: env::Args) -> Result<Self, &'static str> {
         let _name = args.next();
         let query_opt = args.next();
         let filepath_opt = args.next();
         match (query_opt, filepath_opt) {
-            (Some(query), Some(filepath)) => Config { query, filepath },
-            (_, _) => todo!("How should we handle missing args?"),
+            (Some(query), Some(filepath)) => Ok(Config { query, filepath }),
+            (_, _) => Err("expected 2 arguments, query and filepath"),
         }
     }
 }
 
 fn main() {
     let args = env::args();
-    let config = Config::new(args);
+    let config = Config::build(args).unwrap_or_else(|msg| {
+        println!("Error parsing config: {}", msg);
+        process::exit(1);
+    });
     println!("Searching for \"{}\" in file path {}", config.query, config.filepath);
 
     // @graceful: file not found or badly read
