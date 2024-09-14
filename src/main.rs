5bafa2869a265318de962ccef9862d800ebc9805
Doran Penner <doranaviv@gmail.com>
2024-09-14 11:51:53 +0200

    even more idiomatic error handling!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-use std::{env, fs, io};
-
-/* Notes
- * I'm doing the arg parsing differently than the guide,
- * parse_args takes ownership of args and returns true Strings
- */
+use std::error::Error;
+use std::{env, fs};
 
 struct Config {
     query: String,
@@ -11,34 +7,33 @@ struct Config {
 }
 
 impl Config {
-    fn build(mut args: env::Args) -> Result<Self, io::Error> {
-        args.next(); // get rid of executable name
+    fn build(mut args: env::Args) -> Result<Self, Box<dyn Error>> {
+        args.next(); // gets rid of executable name
         let query_opt = args.next();
         let filepath_opt = args.next();
         match (query_opt, filepath_opt) {
             (Some(query), Some(filepath)) => Ok(Config { query, filepath }),
-            // @detail should it be a String? Rust was happy to convert it before...
-            (_, _) => Err(io::Error::new(
-                io::ErrorKind::InvalidInput,
-                "Expected two arguments, query and filepath",
-            )),
+            (_, _) => Err("Expected two arguments, query and filepath".into()),
         }
     }
 }
 
-// @fancy it prints the final error a bit weirdly, what's the best way to do it?
-fn main() -> Result<(), io::Error> {
-    let args = env::args();
-    let config = Config::build(args)?;
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!(
         "Searching for \"{}\" in file path {}",
         config.query, config.filepath
     );
 
-    // @graceful: file not found or badly read
     let contents = fs::read_to_string(config.filepath)?;
 
     println!("contents of file are:\n{contents}");
+    todo!()
+}
 
+// @fancy it prints the final error a bit weirdly, can it be better?
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = env::args();
+    let config = Config::build(args)?;
+    run(config)?;
     Ok(())
 }
