bd48e6c911bc57d056b88ea211bcad05f91135ae
Doran Penner <doranaviv@gmail.com>
2024-09-14 23:28:46 +0200

    got it to work

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,20 @@
 use std::error::Error;
-use std::{fs, iter};
+use std::{fs, iter, env};
 
 pub struct Config {
     query: String,
     filepath: String,
+    sensitive: bool
 }
 
 impl Config {
     pub fn build(mut args: impl Iterator<Item = String>) -> Result<Self, Box<dyn Error>> {
+        let sensitive = env::var("IGNORE_CASE").is_err();
         args.next(); // gets rid of executable name
         let query_opt = args.next();
         let filepath_opt = args.next();
         match (query_opt, filepath_opt) {
-            (Some(query), Some(filepath)) => Ok(Config { query, filepath }),
+            (Some(query), Some(filepath)) => Ok(Config { query, filepath, sensitive }),
             (_, _) => Err("Expected two arguments, query and filepath".into()),
         }
     }
@@ -21,19 +23,27 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.filepath)?;
 
-    for (line_num, line) in search(&config.query, &contents) {
-        println!("{}: {}", line_num, line);
+    // @ugly: can't assigne var `matches` to output of search_ because
+    // they have different output types (silly returning iterator)
+    if config.sensitive {
+        for (line_num, line) in search_sens(&config.query, &contents) {
+            println!("{}: {}", line_num, line);
+        }
+    } else {
+        for (line_num, line) in search_insens(&config.query, &contents) {
+            println!("{}: {}", line_num, line);
+        }
     }
 
     Ok(())
 }
 
-// @optimize: best type of string to pass around?
-pub fn search<'a>(query: &'a str, contents: &'a str) -> impl Iterator<Item = (u32, &'a str)> + 'a {
-    // @learn I'm not sure how I feel about the closure owning query, what does that actually mean?
-    // does it take it from the external Config or just this function's input?
-    // Surely this one, since query was passed in as an immutable reference
+pub fn search_sens<'a>(query: &'a str, contents: &'a str) -> impl Iterator<Item = (u32, &'a str)> + 'a {
     iter::zip(1.., contents.lines()).filter(move |(_, line)| line.contains(query))
+}
+
+pub fn search_insens<'a>(query: &'a str, contents: &'a str) -> impl Iterator<Item = (u32, &'a str)> + 'a {
+    iter::zip(1.., contents.lines()).filter(move |(_, line)| line.to_lowercase().contains(&query.to_lowercase()))
 }
 
 #[cfg(test)]
@@ -49,19 +59,19 @@ Pick three."
             .to_string();
         assert_eq!(
             vec![(2, "safe, fast, productive.")],
-            search("duct", &contents).collect::<Vec<_>>()
+            search_sens("duct", &contents).collect::<Vec<_>>()
         );
         assert_eq!(
             vec![(2, "safe, fast, productive.")],
-            search("safe", &contents).collect::<Vec<_>>()
+            search_sens("safe", &contents).collect::<Vec<_>>()
         );
         assert_eq!(
             vec![(1, "Rust:")],
-            search("R", &contents).collect::<Vec<_>>()
+            search_sens("R", &contents).collect::<Vec<_>>()
         );
         assert_eq!(
             vec![(2, "safe, fast, productive."), (3, "Pick three.")],
-            search(".", &contents).collect::<Vec<_>>()
+            search_sens(".", &contents).collect::<Vec<_>>()
         );
         assert_eq!(
             vec![
@@ -69,7 +79,7 @@ Pick three."
                 (2, "safe, fast, productive."),
                 (3, "Pick three.")
             ],
-            search("t", &contents).collect::<Vec<_>>()
+            search_sens("t", &contents).collect::<Vec<_>>()
         );
     }
 
