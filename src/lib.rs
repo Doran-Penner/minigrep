70bb6c9f7a8c00d7e425bd0d82076845fe11c990
Doran Penner <doranaviv@gmail.com>
2024-09-14 23:46:33 +0200

    Thanks rust compiler :)
    Honestly I kinda keyboard mashed until it worked, but it works! The
    function `search` now handles both case-sensitive and case-insensitive
    searches. My only remaining question in this area is speed --- we turn
    every line into a String for consistent types, and I don't know if
    that'll be a constant memory allocation or if it'll grow linearly with
    the size of the input file. (I want it to be constant.)

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 use std::error::Error;
-use std::{fs, iter, env};
+use std::{env, fs, iter};
 
 pub struct Config {
     query: String,
     filepath: String,
-    sensitive: bool
+    sensitive: bool,
 }
 
 impl Config {
@@ -14,7 +14,11 @@ impl Config {
         let query_opt = args.next();
         let filepath_opt = args.next();
         match (query_opt, filepath_opt) {
-            (Some(query), Some(filepath)) => Ok(Config { query, filepath, sensitive }),
+            (Some(query), Some(filepath)) => Ok(Config {
+                query,
+                filepath,
+                sensitive,
+            }),
             (_, _) => Err("Expected two arguments, query and filepath".into()),
         }
     }
@@ -23,27 +27,34 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.filepath)?;
 
-    // @ugly: can't assigne var `matches` to output of search_ because
-    // they have different output types (silly returning iterator)
-    if config.sensitive {
-        for (line_num, line) in search_sens(&config.query, &contents) {
-            println!("{}: {}", line_num, line);
-        }
-    } else {
-        for (line_num, line) in search_insens(&config.query, &contents) {
-            println!("{}: {}", line_num, line);
-        }
+    for (line_num, line) in search(&config.query, &contents, config.sensitive) {
+        println!("{}: {}", line_num, line);
     }
 
     Ok(())
 }
 
-pub fn search_sens<'a>(query: &'a str, contents: &'a str) -> impl Iterator<Item = (u32, &'a str)> + 'a {
-    iter::zip(1.., contents.lines()).filter(move |(_, line)| line.contains(query))
-}
-
-pub fn search_insens<'a>(query: &'a str, contents: &'a str) -> impl Iterator<Item = (u32, &'a str)> + 'a {
-    iter::zip(1.., contents.lines()).filter(move |(_, line)| line.to_lowercase().contains(&query.to_lowercase()))
+pub fn search<'a>(
+    query: &'a str,
+    contents: &'a str,
+    case_sensitive: bool,
+) -> impl Iterator<Item = (u32, &'a str)> + 'a {
+    iter::zip(1.., contents.lines()).filter(move |(_, line)| {
+        // @optimize: I'm turning every element into a String at some point,
+        // is there a more efficient way to do this? (no no no pls leave it alone)
+        (if case_sensitive {
+            line.to_string()
+        } else {
+            line.to_lowercase()
+        })
+        .contains(
+            &(if case_sensitive {
+                query.to_string()
+            } else {
+                query.to_lowercase()
+            }),
+        )
+    })
 }
 
 #[cfg(test)]
@@ -51,7 +62,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_search() {
+    fn test_search_sens() {
         let contents = "\
 Rust:
 safe, fast, productive.
@@ -59,19 +70,19 @@ Pick three."
             .to_string();
         assert_eq!(
             vec![(2, "safe, fast, productive.")],
-            search_sens("duct", &contents).collect::<Vec<_>>()
+            search("duct", &contents, true).collect::<Vec<_>>()
         );
         assert_eq!(
             vec![(2, "safe, fast, productive.")],
-            search_sens("safe", &contents).collect::<Vec<_>>()
+            search("safe", &contents, true).collect::<Vec<_>>()
         );
         assert_eq!(
             vec![(1, "Rust:")],
-            search_sens("R", &contents).collect::<Vec<_>>()
+            search("R", &contents, true).collect::<Vec<_>>()
         );
         assert_eq!(
             vec![(2, "safe, fast, productive."), (3, "Pick three.")],
-            search_sens(".", &contents).collect::<Vec<_>>()
+            search(".", &contents, true).collect::<Vec<_>>()
         );
         assert_eq!(
             vec![
@@ -79,10 +90,9 @@ Pick three."
                 (2, "safe, fast, productive."),
                 (3, "Pick three.")
             ],
-            search_sens("t", &contents).collect::<Vec<_>>()
+            search("t", &contents, true).collect::<Vec<_>>()
         );
     }
 
-    // @production: I could add more tests for each thing, or extract the read_to_string from run,
-    // or other things
+    // @production: break this test up, add tests for other cases and other functions
 }
